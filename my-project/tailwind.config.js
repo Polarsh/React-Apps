1033fe5f723233e4bc1a4970e15386d5806f39c7
Polar <dybsm98@outlook.es>
2023-04-11 15:52:00 -0500

    configuración faltante

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -8,18 +8,18 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          "primary": "#ff1901",
-          "secondary": "#666666",
-          "accent": "#1FB2A5",
-          "neutral": "#191D24",
-          "base-100": "#2A303C",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
+          primary: "#ff1901",
+          secondary: "#666666",
+          accent: "#1FB2A5",
+          neutral: "#191D24",
+          "base-100": "#ffffff",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
         },
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
 };
